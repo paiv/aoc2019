272564ac8a25272178d836ea2502e443cceb79a7
Pavel Ivashkov <pavels.code@gmail.com>
2019-12-12 23:57:37 +0200

    day 11

--- intcode/intcode/disasm.py
+++ intcode/intcode/disasm.py
@@ -8,11 +8,11 @@ class IntcodeDisasm:
     def _generate(self, mem, ip=0, output=None):
         so = output or io.StringIO()
 
-        def write_line(*args):
-            print(*args, file=so)
+        def write_line(*args, end='\n'):
+            print(*args, end=end, file=so)
 
         def ref(m, a):
-            return str(a) if m else f'[{a}]'
+            return f'[base+{a}]' if m == 2 else f'[{a}]' if m == 0 else str(a)
 
         known_ops = {1, 2, 3, 4, 5, 6, 7, 8, 99}
 
@@ -26,15 +26,19 @@ class IntcodeDisasm:
 
             if op in known_ops:
                 if skipped:
+                    o = ip - len(skipped)
                     for i in range(0, len(skipped), 8):
                         s = ', '.join(map(str, skipped[i:i+8]))
+                        write_line(f'{o:05}:', end=' ')
                         write_line(f'dw {s}')
+                        o += 8
                     skipped = list()
 
             if op == 1:
                 a = mem[ip + 1]
                 b = mem[ip + 2]
                 c = mem[ip + 3]
+                write_line(f'{ip:05}:', end=' ')
                 ip += 4
                 write_line(f'add [{c}], {ref(ma, a)}, {ref(mb, b)}')
 
@@ -42,28 +46,33 @@ class IntcodeDisasm:
                 a = mem[ip + 1]
                 b = mem[ip + 2]
                 c = mem[ip + 3]
+                write_line(f'{ip:05}:', end=' ')
                 ip += 4
                 write_line(f'mul [{c}], {ref(ma, a)}, {ref(mb, b)}')
 
             elif op == 3:
                 a = mem[ip + 1]
+                write_line(f'{ip:05}:', end=' ')
                 ip += 2
                 write_line(f'in [{a}]')
 
             elif op == 4:
                 a = mem[ip + 1]
+                write_line(f'{ip:05}:', end=' ')
                 ip += 2
                 write_line(f'out {ref(ma, a)}')
 
             elif op == 5:
                 a = mem[ip + 1]
                 b = mem[ip + 2]
+                write_line(f'{ip:05}:', end=' ')
                 ip += 3
                 write_line(f'jnz {ref(ma, a)}, {ref(mb, b)}')
 
             elif op == 6:
                 a = mem[ip + 1]
                 b = mem[ip + 2]
+                write_line(f'{ip:05}:', end=' ')
                 ip += 3
                 write_line(f'jz {ref(ma, a)}, {ref(mb, b)}')
 
@@ -71,6 +80,7 @@ class IntcodeDisasm:
                 a = mem[ip + 1]
                 b = mem[ip + 2]
                 c = mem[ip + 3]
+                write_line(f'{ip:05}:', end=' ')
                 ip += 4
                 write_line(f'mov [{c}], {ref(ma, a)} < {ref(mb, b)}')
 
@@ -78,12 +88,20 @@ class IntcodeDisasm:
                 a = mem[ip + 1]
                 b = mem[ip + 2]
                 c = mem[ip + 3]
+                write_line(f'{ip:05}:', end=' ')
                 ip += 4
                 write_line(f'mov [{c}], {ref(ma, a)} == {ref(mb, b)}')
 
+            elif op == 9:
+                a = mem[ip + 1]
+                write_line(f'{ip:05}:', end=' ')
+                ip += 2
+                write_line(f'add base, {ref(ma, a)}')
+
             elif op == 99:
+                write_line(f'{ip:05}:', end=' ')
                 ip += 1
-                write_line('hlt')
+                write_line(f'hlt')
 
             else:
                 skipped.append(mem[ip])
